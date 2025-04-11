43025f3e7067ca3a0a9777c60730186cb1116266
shogo_kaneishi <shogo.kaneishi@jp.toto.com>
2025-04-11 18:05:12 +0900

    ev

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -2,7 +2,7 @@ import { secret } from '@aws-amplify/backend';
 
 export async function GET() {
     console.log('OPENAI_API_KEY:', process.env.OPENAI_API_KEY);
-    return new Response(JSON.stringify({ message: secret('OPENAI_API_KEY') }), {
+    return new Response(JSON.stringify({ message: process.env.OPENAI_API_KEY }), {
       headers: { "Content-Type": "application/json" },
     });
   }
