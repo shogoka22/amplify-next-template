28c55696c9376ceed8c063c8fdb4489feb213b7a
shogo_kaneishi <shogo.kaneishi@jp.toto.com>
2025-04-11 18:20:57 +0900

    set

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,4 @@
-import { openai } from '@ai-sdk/openai';
+import { createOpenAI } from '@ai-sdk/openai';
 import { streamText } from 'ai';
 
 // Allow streaming responses up to 30 seconds
@@ -6,6 +6,11 @@ export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
+
+  const openai = createOpenAI({
+    // custom settings, e.g.
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
   const result = streamText({
     model: openai('gpt-4o'),
