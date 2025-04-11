f172af0a2bec5525b2ceeea559922cfe4955eeec
shogo_kaneishi <shogo.kaneishi@jp.toto.com>
2025-04-11 17:15:37 +0900

    add

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Chat() {
         <input
           className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder={process.env.OPENAI_API_KEY}
           onChange={handleInputChange}
         />
       </form>
